7658203013fb8f444cf0cfafc42a81c5578de2b0
Lisandro Valdez <71908634+lisandrovaldez@users.noreply.github.com>
2026-04-10 15:46:50 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,12 @@ import { JavaScriptLogo } from './assets/JavaScriptLogo'
 import { Start } from './Start'
 import { Game } from './Game'
 import { useQuestionsStore } from './store/questions'
-// import { Final } from './Final'
-// import { useQuestionsData } from './hooks/useQuestionsData'
+import { Final } from './Final'
+import { useQuestionsData } from './hooks/useQuestionsData'
 
 function App() {
   const questions = useQuestionsStore((state) => state.questions)
-  // const { unanswered } = useQuestionsData()
+  const { unanswered } = useQuestionsData()
   return (
     <main>
       <Container placeContent="center" placeItems="center" w="100vw" h="100vh" p={0}>
@@ -20,7 +20,7 @@ function App() {
 
         {questions.length === 0 && <Start />}
         {questions.length > 0 && <Game />}
-        {/* {questions.length > 0 && unanswered === 0 && <Final />} */}
+        /* {questions.length > 0 && unanswered === 0 && <Final />} */
       </Container>
     </main>
   )
